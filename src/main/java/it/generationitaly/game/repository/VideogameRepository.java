eab4898840590359bc4a3481bf9fd3dbea8ba51a
Simone <Simone@DESKTOP-I2O45U8.home>
2024-10-11 11:58:43 +0200

    fatte repository videogame

--- src/main/java/it/generationitaly/game/repository/VideogameRepository.java
+++ src/main/java/it/generationitaly/game/repository/VideogameRepository.java
@@ -1,7 +1,28 @@
 package it.generationitaly.game.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import it.generationitaly.game.entity.Videogame;
 
+/*
+ * CREATE TABLE videogame (
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titolo VARCHAR(255) NOT NULL,
+    descrizione VARCHAR(2048) NOT NULL,
+    data_uscita DATE NOT NULL,
+    classificazione_pegi CHAR(3) NOT NULL,
+    foto VARCHAR(2048) NOT NULL,
+    multiplayer BOOL NOT NULL
+);
+ */
 public interface VideogameRepository extends JpaRepository<Videogame, Long> {
-
+	
+	List<Videogame> RicercaPerTitolo(String titolo);
+	List<Videogame> RicercaPerData(Date dataUscita);
+	List<Videogame> RicercaPerPegi(String classificazionePegi);
+	List<Videogame> RicercaPerMultiplayer(boolean multiplayer);
+	
+	
+	
 }
--- src/main/java/it/generationitaly/game/repository/impl/VideogameRepositoryImpl.java
+++ src/main/java/it/generationitaly/game/repository/impl/VideogameRepositoryImpl.java
@@ -1,13 +1,123 @@
 package it.generationitaly.game.repository.impl;
 
+import java.util.Date;
+import java.util.List;
+
 import it.generationitaly.game.entity.Videogame;
 import it.generationitaly.game.repository.VideogameRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 
 public class VideogameRepositoryImpl extends JpaRepositoryImpl<Videogame, Long> implements VideogameRepository {
 
 	public VideogameRepositoryImpl() {
 		super(Videogame.class);
-		// TODO Auto-generated constructor stub
+		
+	}
+
+	@Override
+	public List<Videogame> RicercaPerTitolo(String titolo) {
+		List<Videogame> videogames=null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		try {
+			em = emf.createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+			TypedQuery<Videogame> query = em.createQuery(
+					"SELECT vid FROM Videogame  WHERE vid.titolo=:titolo",
+					Videogame.class);
+			query.setParameter("titolo", titolo);
+			videogames = query.getResultList();
+			tx.commit();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		} finally {
+			if (em != null)
+				em.close();
+		}
+		return videogames;
+	}
+
+	@Override
+	public List<Videogame> RicercaPerData(Date dataUscita) {
+		List<Videogame> videogames=null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		try {
+			em = emf.createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+			TypedQuery<Videogame> query = em.createQuery(
+					"SELECT vid FROM Videogame  WHERE vid.dataUscita=:dataUscita",
+					Videogame.class);
+			query.setParameter("dataUscita", dataUscita);
+			videogames = query.getResultList();
+			tx.commit();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		} finally {
+			if (em != null)
+				em.close();
+		}
+		return videogames;
+	}
+
+	@Override
+	public List<Videogame> RicercaPerPegi(String classificazionePegi) {
+		List<Videogame> videogames=null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		try {
+			em = emf.createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+			TypedQuery<Videogame> query = em.createQuery(
+					"SELECT vid FROM Videogame  WHERE vid.classificazionePegi=:classificazionePegi",
+					Videogame.class);
+			query.setParameter("classificazionePegi", classificazionePegi);
+			videogames = query.getResultList();
+			tx.commit();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		} finally {
+			if (em != null)
+				em.close();
+		}
+		return videogames;
+	}
+
+	@Override
+	public List<Videogame> RicercaPerMultiplayer(boolean multiplayer) {
+		List<Videogame> videogames=null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		try {
+			em = emf.createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+			TypedQuery<Videogame> query = em.createQuery(
+					"SELECT vid FROM Videogame  WHERE vid.multiplayer=:multiplayer",
+					Videogame.class);
+			query.setParameter("multiplayer", multiplayer);
+			videogames = query.getResultList();
+			tx.commit();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		} finally {
+			if (em != null)
+				em.close();
+		}
+		return videogames;
 	}
 
 }
