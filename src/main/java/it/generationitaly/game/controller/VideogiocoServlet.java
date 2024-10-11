2c2e0f446453996f5bc37497440d382eb3699206
niroxxe <marsicano46@gmail.com>
2024-10-11 17:11:29 +0200

    sviluppo funzione servlet ricerca

--- src/main/java/it/generationitaly/game/controller/VideogiocoServlet.java
+++ src/main/java/it/generationitaly/game/controller/VideogiocoServlet.java
@@ -1,17 +1,18 @@
 package it.generationitaly.game.controller;
 
-import jakarta.servlet.RequestDispatcher;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import it.generationitaly.game.entity.Videogame;
 import it.generationitaly.game.repository.VideogameRepository;
 import it.generationitaly.game.repository.impl.VideogameRepositoryImpl;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class VideogiocoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -21,10 +22,20 @@ public class VideogiocoServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		long id = Long.parseLong(request.getParameter("id"));
-		// System.out.println("videogame.id="+id); 
-		Videogame videogame = videogameRepository.findById(id);
-		// System.out.println(videogame);
+		List<Videogame> videogame = new ArrayList<Videogame>();
+		String titolo = request.getParameter("titolo");
+		String genere = request.getParameter("genere");
+		String sviluppatore = request.getParameter("sviluppatore");
+		String classificazionePegi = request.getParameter("classificazione_pegi");
+		Boolean multiplayer = Boolean.parseBoolean(request.getParameter("multiplayer"));
+		LocalDate date = LocalDate.parse(request.getParameter("anno_uscita"));
+		
+		videogame = videogameRepository.findAll();
+		/*
+		if(titolo != null) {
+			videogame = videogameRepository.RicercaPerTitolo(titolo);
+		}
+		*/
 		request.setAttribute("videogame", videogame);
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("videogame.jsp");
 		requestDispatcher.forward(request, response);
