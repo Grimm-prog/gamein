e0a0f24829ba70615477e9504136af0c57528a37
crist <crist@Cristian.homenet.telecomitalia.it>
2024-10-11 12:42:37 +0200

    committiamo

--- src/main/java/it/generationitaly/game/repository/UtenteRepository.java
+++ src/main/java/it/generationitaly/game/repository/UtenteRepository.java
@@ -1,7 +1,12 @@
-	package it.generationitaly.game.repository;
+package it.generationitaly.game.repository;
 
+import java.util.List;
+
+import it.generationitaly.game.entity.Preferito;
 import it.generationitaly.game.entity.Utente;
+import it.generationitaly.game.entity.Videogame;
 
 public interface UtenteRepository extends JpaRepository<Utente, Long> {
 
+	List<Videogame> preferiti(Utente utente, Preferito preferito);
 }
