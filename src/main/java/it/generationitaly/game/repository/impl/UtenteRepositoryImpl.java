1e5fd5870f0f90e3aa0629b7e5681eacced8ecda
Simone <Simone@DESKTOP-I2O45U8.home>
2024-10-11 16:55:55 +0200

    utente ricerca per user

--- src/main/java/it/generationitaly/game/repository/impl/UtenteRepositoryImpl.java
+++ src/main/java/it/generationitaly/game/repository/impl/UtenteRepositoryImpl.java
@@ -8,6 +8,7 @@ import it.generationitaly.game.entity.Videogame;
 import it.generationitaly.game.repository.UtenteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 
 public class UtenteRepositoryImpl extends JpaRepositoryImpl<Utente, Long> implements UtenteRepository {
@@ -42,6 +43,30 @@ public class UtenteRepositoryImpl extends JpaRepositoryImpl<Utente, Long> implem
 		}
 		System.out.println("ciao");
 		return preferiti;
+	}
+
+	@Override
+	public Utente findByUsername(String username) {
+		Utente utente=null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		try {
+			em = emf.createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+			 TypedQuery<Utente> query = em.createQuery("SELECT u FROM Utente u WHERE u.username=:username", Utente.class);
+			query.setParameter("username", username);
+			utente = query.getSingleResult();
+			tx.commit();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		} finally {
+			if (em != null)
+				em.close();
+		}
+		return utente;
 	}
 
 	
