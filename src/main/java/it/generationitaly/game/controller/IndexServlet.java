3d7df824e084feda3d7ee98b190e56a068618139
crist <crist@Cristian.homenet.telecomitalia.it>
2024-10-14 19:25:25 +0200

    fixato metodo orderByDataDesc, messo il metodo nella servlet index al posto della findAll, e aggiornato la foto dell' insert (id:10)

--- src/main/java/it/generationitaly/game/controller/IndexServlet.java
+++ src/main/java/it/generationitaly/game/controller/IndexServlet.java
@@ -20,7 +20,7 @@ public class IndexServlet extends HttpServlet {
 	private VideogameRepository videogameRepository = new VideogameRepositoryImpl();
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Videogame> videogames = videogameRepository.findAll();
+		List<Videogame> videogames = videogameRepository.OrderByDataDesc();
 		request.setAttribute("videogames", videogames);
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
 		requestDispatcher.forward(request, response);
