6f908402288447c418b1b2a4b0be4222300921e3
Simone <Simone@DESKTOP-I2O45U8.home>
2024-10-14 11:34:32 +0200

    cambi alle servlet

--- src/main/java/it/generationitaly/game/controller/VideogiocoServlet.java
+++ src/main/java/it/generationitaly/game/controller/VideogiocoServlet.java
@@ -22,21 +22,8 @@ public class VideogiocoServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		List<Videogame> videogame = new ArrayList<Videogame>();
-		String titolo = request.getParameter("titolo");
-		String genere = request.getParameter("genere");
-		String sviluppatore = request.getParameter("sviluppatore");
-		String classificazionePegi = request.getParameter("classificazione_pegi");
-		Boolean multiplayer = Boolean.parseBoolean(request.getParameter("multiplayer"));
-		LocalDate date = LocalDate.parse(request.getParameter("anno_uscita"));
-		
-		videogame = videogameRepository.findAll();
-		/*
-		if(titolo != null) {
-			videogame = videogameRepository.RicercaPerTitolo(titolo);
-		}
-		*/
-		//
+		long id = Long.parseLong(request.getParameter("id"));
+		Videogame videogame = videogameRepository.findById(id);
 		request.setAttribute("videogame", videogame);
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("videogame.jsp");
 		requestDispatcher.forward(request, response);
