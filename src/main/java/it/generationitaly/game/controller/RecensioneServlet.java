72cde32c4dfc7c240c8c977943665ec7415d03a2
Simone <Simone@DESKTOP-I2O45U8.home>
2024-10-17 14:25:39 +0200

    recensione servlet

---
+++ src/main/java/it/generationitaly/game/controller/RecensioneServlet.java
@@ -0,0 +1,54 @@
+package it.generationitaly.game.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import com.mysql.cj.Session;
+
+import it.generationitaly.game.entity.Recensione;
+import it.generationitaly.game.entity.Utente;
+import it.generationitaly.game.entity.Videogame;
+import it.generationitaly.game.repository.RecensioneRepository;
+import it.generationitaly.game.repository.VideogameRepository;
+import it.generationitaly.game.repository.impl.RecensioneRepositoryImpl;
+import it.generationitaly.game.repository.impl.VideogameRepositoryImpl;
+
+@WebServlet("/recensione")
+public class RecensioneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private RecensioneRepository recensioneRepository= new RecensioneRepositoryImpl();
+	private VideogameRepository videogameRepository= new VideogameRepositoryImpl();
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Utente utente=(Utente)session.getAttribute("username");
+		long idVideogioco = Long.parseLong(request.getParameter("id"));
+		String commento = request.getParameter("commento");
+		int voto=Integer.parseInt(request.getParameter("voto"));
+		int difficolta=Integer.parseInt(request.getParameter("difficolta"));
+		int tempoDiGioco=Integer.parseInt(request.getParameter("tempoDiGioco"));
+		Videogame videogame = videogameRepository.findById(idVideogioco);
+		Recensione recensione= new Recensione();
+		recensione.setCommento(commento);
+		recensione.setVoto(voto);
+		recensione.setDifficolta(difficolta);
+		recensione.setTempoDiGioco(tempoDiGioco);
+		recensione.setUtente(utente);
+		recensione.setVideogame(videogame);
+		recensioneRepository.save(recensione);
+		
+		request.setAttribute("id", idVideogioco);
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("videogioco");
+		requestDispatcher.forward(request, response);
+		
+		
+	}
+
+}
